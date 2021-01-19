cbf3d27d59c79f51aa80f9573d224fbbd3c25f0e
e-floran <e-floran@hotmail.fr>
2021-01-19 21:55:13 +0100

    made app work with swiper in it

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,18 +1,35 @@
 <template>
   <div v-if="loaded" class="product-card">
-    <div v-if="images.length > 0">
+    <!-- <div v-if="images.length > 0">
       <viewer :images="images" :options="imageZoomOptions">
         <img :src="url" v-for="url in images" :key="url" loading="lazy" />
       </viewer>
       <div class="ui divider"></div>
-    </div>
+    </div> -->
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+      <swiper-slide>Slide 5</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
   </div>
 </template>
 
 <script>
 import offService from "../off";
+import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 export default {
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  directives: {
+    swiper: directive
+  },
   name: "Product",
   props: ["barcode"],
   data: function () {
@@ -24,6 +41,11 @@ export default {
           rotateLeft: 1,
           rotateRight: 1,
         },
+      },
+      swiperOptions: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
       },
     };
   },
@@ -45,10 +67,17 @@ export default {
       });
     },
   },
+  computed: {
+      swiper() {
+        return this.$refs.mySwiper.$swiper
+      }
+    },
   mounted() {
     if (this.barcode) {
       this.update();
     }
+    console.log('Current Swiper instance object', this.swiper)
+      this.swiper.slideTo(3, 1000, false)
   },
 };
 </script>
