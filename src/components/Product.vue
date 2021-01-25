115e8607e1f9207f16ccad4603fada391c1653cc
Benoît Hubert <benoithubert@gmail.com>
2021-01-25 09:43:24 +0100

    Use an external swipe module

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,17 +1,18 @@
 <template>
   <div v-show="loaded" class="product-card">
-  <div class='swiperContainer' ref="swiperContainer">
-    <img :src="url" v-for="url in images" :key="url" loading="lazy" />
-  </div>
+  <vue-wild-swipe :images="images"></vue-wild-swipe>
   </div>
 </template>
 
 <script>
+import VueWildSwipe from "vue-wild-swipe";
 import offService from "../off";
-import swipeUtils from "../utils/swipeUtil";
 
 export default {
   name: "Product",
+  components: {
+    VueWildSwipe
+  },
   props: ["barcode"],
   data: function () {
     return {
@@ -44,7 +45,7 @@ export default {
     update: function () {
       offService.getImage(this.barcode).then((result) => {
         this.images = result.data;
-        
+
         this.loaded = true;
       });
     },
@@ -53,40 +54,6 @@ export default {
     if (this.barcode) {
       this.update();
     }
-  },
-  updated(){
-    const swiperContainer = this.$refs.swiperContainer;
-    if (this.loaded && this.images.length > 0 && !this.eventListeners) {
-
-      this.eventListeners = swipeUtils(swiperContainer);
-      const { size, lock, drag, move } = this.eventListeners;
-      
-      addEventListener("resize", size, false);
-
-      swiperContainer.addEventListener("mousedown", lock, false);
-      swiperContainer.addEventListener("touchstart", lock, false);
-
-      swiperContainer.addEventListener("mousemove", drag, false);
-      swiperContainer.addEventListener("touchmove", drag, false);
-
-      swiperContainer.addEventListener("mouseup", move, false);
-      swiperContainer.addEventListener("touchend", move, false);
-    }
-  },
-  beforeDestroy() {
-    const swiperContainer = this.$refs.swiperContainer;
-    const { size, lock, drag, move } = this.eventListeners;
-
-    removeEventListener("resize", size, false);
-
-    swiperContainer.removeEventListener("mousedown", lock, false);
-    swiperContainer.removeEventListener("touchstart", lock, false);
-
-    swiperContainer.removeEventListener("mousemove", drag, false);
-    swiperContainer.removeEventListener("touchmove", drag, false);
-
-    swiperContainer.removeEventListener("mouseup", move, false);
-    swiperContainer.removeEventListener("touchend", move, false);
   }
 };
 </script>
