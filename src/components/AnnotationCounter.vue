a86a450218eb1abca95a89c239d344391dcabc5e
e-floran <e-floran@hotmail.fr>
2021-01-26 20:47:15 +0100

    working on logic

--- src/components/AnnotationCounter.vue
+++ src/components/AnnotationCounter.vue
@@ -4,7 +4,7 @@
       {{ $t("game.next_level") }}: {{ annotatedCount }} /
       {{ levelToReach }}
     </p>
-    <div class="congratContainer">
+    <div class="congratContainer" v-if="showPopUp">
         <p class="congrat">Bravo</p>
   </div>
   </article>
@@ -27,18 +27,32 @@ export default {
       type: String,
       required: true,
     },
+    popUpMessages: {
+      type: Array,
+      default (){
+        return [
+      "Wow", "Amazing", "Wonderful", "Impressive", "Great job", "Bravo", "Sensational", "Tremendous", "Wondrous", "Marvelous", "Prodigious", "Stupendous", "Phenomenal", "Colossal", "Legendary"
+    ]},},
+    allRanks:{
+      type: Array,
+      default (){
+        return [
+      20, 40, 60, 80, 100, 200, 300, 400, 500, 1000, 1500, 2000, 3000, 4000, 5000, 7500, 10000, 25000, 50000, 100000, 150000, 200000, 500000, 1000000, 10000000, 100000000, 1000000000, 10000000000
+    ]},},
   },
   data: function() {
     return {
       historyAnnotatedCount: 0,
-      levelToReach: 20,
+      levelToReach: 0,
+      showPopUp: false,
     };
   },
   watch: {
     sessionAnnotatedCount() {
-      if (this.annotatedCount === this.levelToReach) {
-        alert(`Palier ${this.levelToReach} atteint !! Bravo`);
-        this.levelToReach *= 2;
+      if (this.annotatedCount === this.allRanks[this.levelToReach]) {
+        // alert(`Palier ${this.levelToReach} atteint !! Bravo`);
+        this.showPopUp = true;
+        this.levelToReach += 1;
       }
       saveUserInsightLocalStorage(
         this.annotatedCount,
